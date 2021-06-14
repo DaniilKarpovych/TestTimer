da5fbb4c8b03fe9567e8ec8283d787ce13b61442
Symenon <kadavi444@gmail.com>
2021-06-14 12:23:16 +0300

    add App

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { Observable, Subject } from "rxjs";
 import { timestamp, pairwise } from "rxjs/operators";
 
 function App() {
-  const [time, setTime] = useState("00:00:00");
+  const [time, setTime] = useState(0);
   const [subscriptions, setSubscriptions] = useState(null);
   const [waitSub, setWaitSub] = useState(null);
 
@@ -13,7 +13,6 @@ function App() {
     setWaitSub(subject);
     return () => {
       setWaitSub(null);
-      console.log("UseEffect");
     };
   }, []);
 
@@ -21,60 +20,70 @@ function App() {
     if (subscriptions) {
       subscriptions.unsubscribe();
       setSubscriptions(null);
-      setTime("00:00:00");
+      setTime(0);
     } else {
       const timer = new Observable((observer) => {
-        let counter = 0;
+        const date = new Date();
         const intervalId = setInterval(() => {
-          observer.next(counter++);
-        }, 1000);
+          observer.next(date);
+        }, 500);
         return () => {
           clearInterval(intervalId);
         };
       });
       const subscription = timer.subscribe((val) => {
-        setTime(val);
-        console.log(val);
+        setTime(new Date() - val);
       });
       setSubscriptions(subscription);
     }
   }, [subscriptions]);
 
   const onStopClick = useCallback(() => {
-    if (subscriptions){
-    subscriptions.unsubscribe();
-    setTime("00:00:00");
-    const timer = new Observable((observer) => {
-      let counter = 0;
-      const intervalId = setInterval(() => {
-        observer.next(counter++);
-      }, 1000);
-      return () => {
-        clearInterval(intervalId);
-      };
-    });
-    const subscription = timer.subscribe((val) => {
-      setTime(val);
-      console.log(val);
-    });
-    setSubscriptions(subscription);
-  }}, [subscriptions]);
+    if (subscriptions) {
+      subscriptions.unsubscribe();
+      setTime(0);
+      const timer = new Observable((observer) => {
+        const date = new Date();
+        const intervalId = setInterval(() => {
+          observer.next(date);
+        }, 500);
+        return () => {
+          clearInterval(intervalId);
+        };
+      });
+      const subscription = timer.subscribe((val) => {
+        setTime(new Date() - val);
+        console.log(val);
+      });
+      setSubscriptions(subscription);
+    }
+  }, [subscriptions]);
 
   const onWaitClick = useCallback(() => {
-    if (subscriptions){
-    waitSub.next();
-    waitSub.pipe(timestamp(), pairwise()).subscribe((v) => {
-      if (v[1].timestamp - v[0].timestamp <= 300) {
-        subscriptions.unsubscribe();
-        console.log("double click");
-      }
-    });
-  }}, [subscriptions]);
+    if (subscriptions) {
+      waitSub.next();
+      waitSub.pipe(timestamp(), pairwise()).subscribe((v) => {
+        if (v[1].timestamp - v[0].timestamp <= 300) {
+          subscriptions.unsubscribe();
+        }
+      });
+    }
+  }, [subscriptions]);
+
+  console.log(time);
+
+  let sec = new Date(time).getSeconds();
+  if (sec < 10) sec = `${"0" + sec}`;
+  let min = new Date(time).getMinutes();
+  if (min < 10) min = `${"0" + min}`;
+  let hours = new Date(time).getUTCHours();
+  if (hours < 10) hours = `${"0" + hours}`;
+  console.log(hours);
 
   return (
     <div className="timer">
       <h1>Timer</h1>
-      <output>{time}</output>
+      <output>{`${hours + ":" + min + ":" + sec}`}</output>
       <div>
         <button onClick={onStartClick}>Start/Stop</button>
         <button onMouseDown={onWaitClick}>Wait</button>
